aadf5704b47ce6c822d37f53b0207361be11d52b
sobatadjie <ajiunsada@gmail.com>
2020-04-18 19:29:38 +0700

    upload project vue

---
+++ src/router/index.js
@@ -0,0 +1,51 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
+
+Vue.use(VueRouter)
+
+  const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/about',
+    name: 'About',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/product',
+    name: 'Product',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "prouduct" */ '../views/Product.vue')
+  },
+  {
+    path: '/chart',
+    name: 'chart',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "chart" */ '../views/ShoppingChart.vue')
+  },
+  {
+    path: '/success',
+    name: 'success',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "success" */ '../views/Success.vue')
+  }
+]
+
+const router = new VueRouter({
+  routes
+})
+
+export default router
---
+++ src/views/Home.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="home">
+      <HeaderRahmi />
+      <HeroSection />      
+      <WomenBanner />
+      <InstaRahmi />
+      <PartnerRahmi />
+      <FooterRahmi />      
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+import HeaderRahmi from '@/components/HeaderRahmi.vue'
+import HeroSection from '@/components/HeroSection.vue'
+import WomenBanner from '@/components/WomenBanner.vue'
+import InstaRahmi from '@/components/InstaRahmi.vue'
+import PartnerRahmi from '@/components/PartnerRahmi.vue'
+import FooterRahmi from '@/components/FooterRahmi.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    HeaderRahmi,
+    HeroSection,
+    WomenBanner,
+    InstaRahmi,
+    PartnerRahmi,
+    FooterRahmi
+  }
+}
+</script>
